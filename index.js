0037a8bd2a55390de49490689538b6d5df7661fc
mahfuj <mahfujahmad424@gmail.com>
2023-05-18 22:57:22 +0600

    connect to toy-server in express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-// const app = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-// app.use(cors());
-// app.use(express());
+app.use(cors());
+app.use(express());
+
 
 
 
