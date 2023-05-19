64d2d8e0f2729ea0702c66e239666d0c3f53cf5f
mahfuj <mahfujahmad424@gmail.com>
2023-05-19 21:44:26 +0600

    post and get api data done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 console.log(process.env.DB_USER);
@@ -29,13 +29,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       
-      const toyCollection = client.db('aeroplaneDB').collection('toy')
+    const toyCollection = client.db('aeroplaneDB').collection('toy')
+    
+
+    app.get('/addToy', async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
       
       app.post('/addToy', async (req, res) => {
         const addToy = req.body;
-        console.log(addToy)
-        // const result = await toyCollection.insertOne(addToy)
-        // res.send(result)
+        const result = await toyCollection.insertOne(addToy)
+        res.send(result)
       })
 
 
